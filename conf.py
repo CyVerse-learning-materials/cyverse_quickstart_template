92cf0e6a3e9398ed7f011240bab4c358ce04c3e9
JasonJWilliamsNY <JasonJWilliamsNY@gmail.com>
2018-09-28 12:36:49 -0400

    update

--- conf.py
+++ conf.py
@@ -9,8 +9,8 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from misc.cyverse_sphinx_conf import *  # noqa
 
-project = 'YOUR PROJECT NAME HERE as topic_quick_start or topic_tutorial'
-copyright = '2016, CyVerse'
+project = 'YOUR QUICKSTART NAME HERE as topic_quick_start'
+copyright = '2018, CyVerse'
 author = 'Jason Williams'
 version = '1.0'
 release = '1.0'
